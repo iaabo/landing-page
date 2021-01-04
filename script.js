d3e5544cc15f7fa8b383b3e2b565ec2a154aed79
iaabo <insbarroso@gmail.com>
2021-01-04 22:27:19 +0000

    changes in the header + about

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-/* Slideshow animation */
+/* Scroll Sections */
 
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
+
+
+/* Slideshow animation */
 
 var slideIndex = 0;
 showSlides();
